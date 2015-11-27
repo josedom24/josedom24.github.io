a9ccc56117b77dc56994694e571c84368a5e752b
José Domingo Muñoz <josedom24@gmail.com>
2015-11-27 11:09:23 +0100

    tomcat

--- mod/iaw/tomcat.py
+++ mod/iaw/tomcat.py
@@ -6,6 +6,7 @@ import os
 USUARIOS=12
 PS_INI=8006
 P_INI=8081
+DIR="/home/"
 
 puertos=[]
 for u in xrange(0,USUARIOS):
@@ -13,4 +14,8 @@ for u in xrange(0,USUARIOS):
 	P_INI=P_INI+1
 	PS_INI=PS_INI+1
 
-print puertos
+cont=1
+for puerto,puerto_sh in puertos:
+	os.system("tomcat7-instance-create -p %i -c %i %s" % (puerto,puerto_sh,DIR+"tomcat-"+str(cont)))
+	cont=cont+1
+
