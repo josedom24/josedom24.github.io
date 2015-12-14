1e28e1a9417638c9fc664233dbcb4c96b279a234
José Domingo Muñoz <josedom24@gmail.com>
2015-12-14 09:51:38 +0100

    Cambio tiempo.py

--- mod/lm/tiempo.py
+++ mod/lm/tiempo.py
@@ -4,14 +4,14 @@
 import urllib2
 response = urllib2.urlopen('http://openweathermap.org/city/2518794')
 lineas=response.readlines()
-lineas=lineas[968:987]
+lineas=lineas[968:989]
 print "Temperatura:"
-inicio=lineas[0].find("png")+6
-fin=lineas[0].find("C</")+1
-print lineas[0][inicio:fin]
+inicio=lineas[2].find("png")+6
+fin=lineas[2].find("C</")+1
+print lineas[2][inicio:fin]
 print "Presión:"
-print lineas[14].split("<td>")[2].split("<")[0]
+print lineas[16].split("<td>")[2].split("<")[0]
 print "Humedad"
-fin=lineas[17].find("%")+1
-inicio=lineas[17].find("</td><td>")+len("</td><td>")
-print lineas[17][inicio:fin]
+fin=lineas[19].find("%")+1
+inicio=lineas[19].find("</td><td>")+len("</td><td>")
+print lineas[19][inicio:fin]
